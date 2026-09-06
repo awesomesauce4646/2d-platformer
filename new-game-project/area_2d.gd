1e48e5d3140faa27b112e41fb27f1460a66c522f
awesomesauce4646 <mary_grellmann@yahoo.com>
2026-09-06 18:56:44 -0500

    init commit

---
+++ new-game-project/area_2d.gd
@@ -0,0 +1,10 @@
+extends Area2D
+
+func _ready() -> void:
+	# Connect the collision signal
+	body_entered.connect(_on_body_entered)
+
+func _on_body_entered(body: Node2D) -> void:
+	# Check if the object entering the door is the Player
+	if body.is_in_group("Player") or body.name == "Player":
+			get_tree().change_scene_to_file("res://exit.tscn")
