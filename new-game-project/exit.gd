d5d7322f72f4655261a36f63bb50a3ffbeebed99
awesomesauce4646 <mary_grellmann@yahoo.com>
2026-09-06 19:00:45 -0500

    main menu added

--- new-game-project/exit.gd
+++ new-game-project/exit.gd
@@ -16,4 +16,4 @@ func _on_button_pressed() -> void:
 
 
 func _on_button_2_pressed() -> void:
-	get_tree().change_scene_to_file("res://main.tscn")
+	get_tree().change_scene_to_file("res://main.scn")
